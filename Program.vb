a2365e663095c30778464999dc2f25303c42fdfb
Lubainapoona <153352618+Lubainapoona@users.noreply.github.com>
2023-12-10 18:10:44 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -2,29 +2,33 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim str1, alphabets As String
-        Dim alpha As Boolean
+        Dim string1, string2 As String
+        Dim char1, char2, char3 As Char
         Dim i As Integer
-        Dim chr1 As Char
-        alphabets = "abcdefghijklmnopqrstuvwxyz"
+        string2 = ""
+        string1 = ""
+        char1 = ""
+        char2 = ""
+        char3 = ""
         i = 0
-        alpha = True
-        chr1 = ""
-        str1 = ""
-        Console.Write("enter string to process: ")
-        str1 = Console.ReadLine
-        str1 = LCase(str1)
-        For i = 1 To Len(alphabets)
-            chr1 = Mid(alphabets, i, 1)
-            If InStr(str1, chr1) = 0 Then alpha = False
-            If alpha = False Then Exit For
-        Next i
-        If alpha = True Then
-            Console.WriteLine(" entered string is alpha ")
-        Else
-            Console.WriteLine("entered string is not alpha")
-        End If
+        Console.Write("enter string to be processed: ")
+        string1 = Console.ReadLine
+        Console.Write("enter character to select in the string: ")
+        char1 = Console.ReadLine
+        Console.Write("enter character to replace in the string: ")
+        char2 = Console.ReadLine
+        For i = 1 To Len(string1)
+            char3 = Mid(string1, i, 1)
+            If char3 = char1 Then
+                string2 = string2 + char2
+            Else
+                string2 = string2 + char3
+            End If
+        Next
+        Console.WriteLine("processed string after replacement is: " & string2)
         Console.ReadKey()
+
+
 
     End Sub
 End Module

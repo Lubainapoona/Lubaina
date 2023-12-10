5600b43a147b904fd05b281bc8310ff52d461ede
Lubainapoona <153352618+Lubainapoona@users.noreply.github.com>
2023-12-10 18:15:19 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -2,15 +2,26 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim numberofdonuts As Integer
-        numberofdonuts = 0
-        Console.Write(" enter the number of dounts:")
-        numberofdonuts = Console.ReadLine
-        If numberofdonuts >= 10 Then
-            Console.WriteLine("number of donuts: many")
+        Dim char1, char2, char3 As Char
+        Dim string1 As String
+        Dim i As Integer
+        i = 0
+        char1 = ""
+        char2 = ""
+        char3 = ""
+        string1 = ""
+
+        Console.Write("enter string: ")
+        string1 = Console.ReadLine
+
+        If Len(string1) > 1 Then
+            Console.WriteLine(Left(string1, 2) & Right(string1, 2))
         Else
-            Console.WriteLine("number of donuts: numberofdonuts")
+            Console.WriteLine(string1)
         End If
         Console.ReadKey()
+
+
+
     End Sub
 End Module

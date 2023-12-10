497465c142eed089c7a474039dbb5250bfe2d18f
Lubainapoona <153352618+Lubainapoona@users.noreply.github.com>
2023-12-10 18:14:21 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -2,46 +2,47 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim str1, str2 As String
-        Dim chr1, chr2, chr3 As Char
-        Dim i, counteralphabets, countentered, countdigits, countother As Integer
-        Dim alphabets As String
-        Dim alphabet As Boolean
-        alphabet = True
-        str1 = ""
-        str2 = ""
-        chr1 = ""
-        chr2 = ""
-        chr3 = ""
+        Dim string1 As String
+        Dim char1, char2 As Char
+        Dim i, countofA, countofE, countofI, countofU, countofO As Integer
+        string1 = ""
+        char1 = ""
+        char2 = ""
         i = 0
-        counteralphabets = 0
-        countentered = 0
-        countdigits = 0
-        countother = 0
-        alphabets = "abcdefghijklmnopqrstuvwxyz"
-        Console.Write("enter string to be processed: ")
-        str1 = Console.ReadLine
-        Console.Write("enter character to be counted:")
-        chr1 = Console.ReadLine
-        LCase(str1)
-        For i = 1 To Len(str1)
-            chr2 = Mid(str1, i, 1)
-            If Asc(chr2) > 47 And Asc(chr2) < 59 Then
-                countdigits = countdigits + 1
-            ElseIf asc(chr2) > 96 And Asc(chr2) < 123 Then
-                counteralphabets = counteralphabets + 1
-            Else
-                countother = countother + 1
-            End If
-            If chr2 = chr1 Then
-                countentered = countentered + 1
+        countofA = 0
+        countofE = 0
+        countofI = 0
+        countofO = 0
+        countofU = 0
+        Console.Write("enter string: ")
+        string1 = Console.ReadLine
+
+        LCase(string1)
+        For i = 1 To Len(string1)
+            char2 = Mid(string1, i, 1)
+            If Asc(char2) = 97 Then
+                countofA = countofA + 1
+            ElseIf Asc(char2) = 101 Then
+                countofE = countofE + 1
+            ElseIf Asc(char2) = 105 Then
+                countofI = countofI + 1
+            ElseIf Asc(char2) = 111 Then
+                countofO = countofO + 1
+            ElseIf Asc(char2) = 115 Then
+                countofU = countofU + 1
             End If
         Next
-        Console.WriteLine("character was entered: " & countentered & "times")
-        Console.WriteLine("alphabets are:" & counteralphabets)
-        Console.WriteLine("digits are:" & countdigits)
-        Console.WriteLine("other keys are: " & countother)
+        Console.WriteLine("a: " & countofA & " times")
+        Console.WriteLine("e: " & countofE & " times")
+        Console.WriteLine("i: " & countofI & " times")
+        Console.WriteLine("o: " & countofO & " times")
+        Console.WriteLine("u: " & countofU & " times")
+
         Console.ReadKey()
+
+
+
+
 
 
 

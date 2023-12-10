bd8c0d2d23154c26f33d7736890fd96ade32f562
Lubainapoona <153352618+Lubainapoona@users.noreply.github.com>
2023-12-10 18:15:52 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -2,25 +2,34 @@ Imports System
 
 Module Program
     Sub Main()
+        Dim string1, string2 As String
         Dim char1, char2, char3 As Char
-        Dim string1 As String
         Dim i As Integer
         i = 0
+        string1 = ""
+        string2 = ""
         char1 = ""
         char2 = ""
         char3 = ""
-        string1 = ""
 
         Console.Write("enter string: ")
         string1 = Console.ReadLine
 
-        If Len(string1) > 1 Then
-            Console.WriteLine(Left(string1, 2) & Right(string1, 2))
-        Else
-            Console.WriteLine(string1)
-        End If
-        Console.ReadKey()
+        char1 = Left(string1, 1)
+        string2 = string2 & char1
 
+        For i = 2 To Len(string1)
+            char2 = Mid(string1, i, 1)
+            If char2 = char1 Then
+                char2 = "*"
+                string2 = string2 & char2
+            Else : char3 = char2
+                string2 = string2 & char3
+            End If
+        Next
+
+        Console.WriteLine(string2)
+        Console.ReadKey()
 
 
     End Sub
